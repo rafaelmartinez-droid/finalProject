29aea0c10f0fa8c24aa6ec14ce3e49a9fa1ac39b
rafaelmartinez <rafaelmartinez@vancouvercollege.ca>
2026-08-13 09:49:03 -0700

    changed documents name using +

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if file and st.button("Process File"):
     now = datetime.now()
     chroma_client = chromadb.Client() #"A" + str(now) + "documents" + "A"
 
-    collection = chroma_client.create_collection("documents", chunks, file.name)
+    collection = chroma_client.create_collection("documents" + file.name)
     tags = [file.name + str(i) for i in range(len(chunks))] #better citations
     collection.add(documents=chunks, ids=tags)
 
