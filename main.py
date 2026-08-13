a80953a1290d095afc2a41ec3e196971642a4c79
rafaelmartinez <rafaelmartinez@vancouvercollege.ca>
2026-08-13 14:21:33 -0700

    undid multiple files

--- main.py
+++ main.py
@@ -24,19 +24,18 @@ with st.sidebar:
     st.write("current distance limiter:", distance_limiter)
 
 #file = st.file_uploader("Upload a .txt file", type="txt")#change
-files = st.file_uploader("Upload a .pdf file or a .txt file", type=["pdf", "txt"], accept_multiple_files=True)#change
+file = st.file_uploader("Upload a .pdf file or a .txt file", type=["pdf", "txt"])#change
 
-if files and st.button("Process File"):
+if file and st.button("Process File"):
     st.write("File processed")
-    for file in files:
-        st.write(file.type)
-        if file.type == "application/pdf":
-            reader = PdfReader(file)
-            text = ""
-            for page in reader.pages:
-                text += page.extract_text() + "\n"
-        elif file.type == "text/plain":
-            text = file.read().decode("utf-8")
+    st.write(file.type)
+    if file.type == "application/pdf":
+        reader = PdfReader(file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+    elif file.type == "text/plain":
+        text = file.read().decode("utf-8")
     chunks = []
     # chunk_size = 300
     # overlap = 100
