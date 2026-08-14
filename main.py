004a6f57954656deb33ef0c630898bab47975b22
rafaelmartinez <rafaelmartinez@vancouvercollege.ca>
2026-08-14 09:26:21 -0700

    added accepted answers

--- main.py
+++ main.py
@@ -86,6 +86,8 @@ if st.button("Search"):
         st.session_state.question = question
         if print_distances:
             st.write(result["distances"])
+            st.write("accepted answers:", len(result["distances"]))
+            st.session_state.context = result["distances"]
         # st.write(st.session_state.context)
         # for ans in st.session_state.context:
         #     st.write(ans)
